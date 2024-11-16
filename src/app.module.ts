b7fcf19d1f0660af0978bc05d5fc1d22ef5fae9f
Kamal <mraryalkamal2011@gmail.com>
2024-11-16 13:57:39 +0545

    project and modules setup

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
+import { TenantModule } from './tenant/tenant.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal:true,
+      load:[configuration]
+    }),
+    MongooseModule.forRootAsync({
+      imports:[ConfigModule],
+      useFactory:async(config)=>({
+        uri:config.get("MONGODB_URL")
+      }),
+      inject:[ConfigService]
+    }),
+    ProductModule,
+    TenantModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
